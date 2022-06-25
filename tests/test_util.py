9de916f1c579cae310ab04be9849c78eacea4ac7
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2022-06-25 20:17:15 +0300

    add github ci tool to run tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -3,9 +3,9 @@ import pandas as pd
 import sys, os
  
 sys.path.append(os.path.abspath(os.path.join('../..')))
+sys.path.append(os.path.abspath(os.path.join('../src/pypoint/scripts')))
 
-from src import pypoint
-from extract_dataframe import TweetDfExtractor
+from util import Util
 
 class TestUtils(unittest.TestCase):
     """
@@ -17,12 +17,17 @@ class TestUtils(unittest.TestCase):
     """
 
     def setUp(self) -> pd.DataFrame:
-        self.util = pypoint.scripts.util.Util()
+        self.util = Util()
         # tweet_df = self.df.get_tweet_df()         
 
 
-    def test_find_statuses_count(self):
-        self.assertEqual(self.df.find_statuses_count(), [8606, 119, 14551, 6231, 42906])
+    def test_convert_epsg(self):
+        pass
+        p3857 = [-10423171.940, 5164494.710]
+        p4326 = [-93.63294662707723, 42.02346282729469]
+        
+        value = self.util.convert_EPSG(3857, 4326, p3857[0], p3857[1])
+        self.assertEqual(value, p4326)
 
 if __name__ == '__main__':
     unittest.main()
