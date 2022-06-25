5e5e3bb22c0b116a59c40250d2f905030dd40380
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2022-06-25 12:40:05 +0000

    util and pypoint complete

--- src/pypoint/scripts/util.py
+++ src/pypoint/scripts/util.py
@@ -84,70 +84,49 @@ class Util:
 
         return [list(selection_list.keys()), list(selection_list.values())]
 
-    def convert_EPSG(self, fromT, lon, lat):
-        """ a function that converts one EPSG format to another
 
-        parameters:
-            fromT: the original EPSG format
-            lon: the longitude value
-            lat: the latitude value
 
-        Return: 
-            [x, y]: a list with new EPSG formatted values
 
-        """
-        P3857 = Proj(init='epsg:3857')
-        P4326 = Proj(init='epsg:4326')
-        if(fromT == 4326):
-            input1 = P4326
-            input2 = P3857
-        else:
-            input1=p3857
-            input2=p4326
-
+    def convert_EPSG(self, fromT, toT, lon, lat):
+        input1 = Proj(init=f'epsg:{fromT}')
+        input2 = Proj(init=f'epsg:{toT}') 
         x, y = transform(input1,input2, lon, lat)
         return [x, y]
-
-    def loop_EPSG_converter(self, listin):
-        """ runs the EPSG converter for specified amount of iterations
-
-        Parameter:
-            listin: a list that contains a pair of latitude and longitude values
-
-        Return: 
-            converted: a new list with converted set of pairs of the values
-
-        """
+        
+    def loop_EPSG_converter(self, listin, fromT, toT):
         converted = []
         for item in listin:
-            converted.append(convert_EPSG(4326, item[0], item[1]))
-
+            converted.append(self.convert_EPSG(fromT, toT, item[0], item[1]))
+            
         return converted
-
 
     def generate_polygon(self, coor, epsg):
         polygon_g = Polygon(coor)
         crs = {'init': 'epsg:'+str(epsg)}
-        polygon = gpd.GeoDataFrame(index=[0], crs=crs, geometry=[polygon_g])       
+        polygon = geopandas.GeoDataFrame(index=[0], crs=crs, geometry=[polygon_g])       
+        
         return polygon
 
 
     def show_on_map(self, polygon, zoom):
         #region selection
-        poly = mapping((polygon2.iloc[:,0][0]))
+        poly = mapping((polygon.iloc[:,0][0]))
         tmp = poly['coordinates'][0][0]
         anchor = [tmp[1], tmp[0]]
         m = folium.Map(anchor,zoom_start=zoom, tiles='cartodbpositron')
         folium.GeoJson(polygon).add_to(m)
         folium.LatLngPopup().add_to(m)
+        
         return m
 
 
-    def modify_pipe_json(self, json_loc, url, region, in_epsg, out_epsg):
-        dicti = read_json(json_loc)
-        dicti['pipeline'][0]['polygon'] = str(polygon.iloc[:,0][0])
-        dicti['pipeline'][0]['filename'] = f"{url}/{region}/ept.json"
+    def modify_pipe_json(self, json_loc, url, in_epsg, out_epsg, polygon_b, bounds=None):
+        dicti = self.read_json(json_loc)
+        if bounds is not None:
+            dicti['pipeline'][0]['bounds'] = f"([{bounds[0]},{bounds[2]}],[{bounds[1]},{bounds[3]}])"
+        dicti['pipeline'][0]['polygon'] = str(polygon_b.iloc[:,0][0])
+        dicti['pipeline'][0]['filename'] = url
         dicti['pipeline'][2]['in_srs'] = f"EPSG:{in_epsg}"
         dicti['pipeline'][2]['out_srs'] = f"EPSG:{out_epsg}"
-        print(dicti)
+        
         return dicti
